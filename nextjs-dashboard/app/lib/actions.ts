d5352c38928571a649cb85376c0a49a019acf63e
Gendi kinji <kithinjigeorge192@gmail.com>
2025-03-04 13:41:18 +0300

    Added authentication and metadata

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -3,6 +3,8 @@ import { z } from 'zod';
 import postgres  from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 //import Error from '../dashboard/invoices/errors';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 const FormSchema = z.object({
@@ -25,7 +27,24 @@ export type State = {
   message?: string;
 
 }
- 
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
 export async function createInvoice(prevState: State, formData: FormData) {
     const validatedFields= CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
